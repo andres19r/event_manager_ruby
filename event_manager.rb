7f37a5f63ce42dce90643ad8a206444f99bf96a3
Andres Rivero <andres19rivero@gmail.com>
2021-07-31 22:18:09 -0400

    Switching over to use the CSV library

--- event_manager.rb
+++ event_manager.rb
@@ -1,17 +1,8 @@
+require 'csv'
 puts 'EventManager Initialized!'
 
-# Read the file contents
-# puts File.exists?('event_attendees.csv')
-# contents = File.read('event_attendees.csv')
-# puts contents
-
-# Read the file line by line
-# Display the first names of all attendees
-lines = File.readlines('event_attendees.csv')
-lines.each_with_index do |line, index|
-# Skipping the header row
-  next if index == 0
-  columns = line.split(",")
-  name = columns[2]
-  p name
+contents = CSV.open('event_attendees.csv', headers: true)
+contents.each do |row|
+  name = row[2]
+  puts name
 end
