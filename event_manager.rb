e59c99379e165ef6f37e8ef16469d9baeedea83c
Andres Rivero <andres19rivero@gmail.com>
2021-07-31 21:23:12 -0400

    Skipping the header row

--- event_manager.rb
+++ event_manager.rb
@@ -9,6 +9,8 @@ puts 'EventManager Initialized!'
 # Display the first names of all attendees
 lines = File.readlines('event_attendees.csv')
 lines.each do |line|
+# Skipping the header row
+  next if line == " ,RegDate,first_Name,last_Name,Email_Address,HomePhone,Street,City,State,Zipcode\n"
   columns = line.split(",")
   name = columns[2]
   p name
