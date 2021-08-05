b819c136731039f97d3b790bb4dc768ab4642c87
Andres Rivero <andres19rivero@gmail.com>
2021-08-05 18:00:10 -0400

    feat: clean phone numbers

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -3,7 +3,7 @@ require 'google/apis/civicinfo_v2'
 require 'erb'
 
 def clean_zipcode(zipcode)
-  zipcode.to_s.rjust(5,'0')[0,5]
+  zipcode.to_s.rjust(5, '0')[0, 5]
 end
 
 def legislators_by_zipcode(zip)
@@ -31,6 +31,10 @@ def save_thank_you_letter(id, form_letter)
   end
 end
 
+def clean_phone_number(phone)
+
+end
+
 puts 'EventManager Initialized!'
 
 template_letter =  File.read('form_letter.erb')
@@ -45,12 +49,21 @@ contents = CSV.open(
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
-
   zipcode = clean_zipcode(row[:zipcode])
+
+  phone = row[:homephone]
+  phone_numbers = 0
+  0.upto(phone.length - 1) do |i|
+    phone_numbers += 1 if phone[i] !~ /\D/
+  end
+  phone = 'bad number' if phone_numbers < 10 || phone_numbers > 11
+  if phone_numbers == 11
+    phone = phone[0] == '1' ? phone[1, 10] : 'bad number'
+  end
 
   legislators = legislators_by_zipcode(zipcode)
 
   form_letter = erb_template.result(binding)
 
-  save_thank_you_letter(id, form_letter)
+  # save_thank_you_letter(id, form_letter)
 end
