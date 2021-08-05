b5008bb4200a1370819671d2064bdddd27928b63
Andres Rivero <andres19rivero@gmail.com>
2021-08-05 16:20:28 -0400

    Outputting form letters to a file

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -33,6 +33,7 @@ contents = CSV.open(
 )
 
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
 
   zipcode = clean_zipcode(row[:zipcode])
@@ -41,5 +42,12 @@ contents.each do |row|
 
   form_letter = erb_template.result(binding)
 
-  puts form_letter
+  Dir.mkdir('output') unless Dir.exist?('output')
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
+
 end
